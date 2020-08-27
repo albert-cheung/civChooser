5ed6e22895cafeaf22cda6e0c62054850af4b245
albert-cheung <43798492+albert-cheung@users.noreply.github.com>
2020-08-27 17:55:32 -0500

    Adding support for more than one person.

--- picker.py
+++ picker.py
@@ -3,5 +3,9 @@ import random
 #List of civilizations
 civilizations = ["Aztecs","Berbers","Britons","Bulgarians","Burmese","Byzantines","Celts","Chinese","Cumans","Ethiopians","Franks","Goths","Huns","Incas","Indians","Italians","Japanese","Khmer","Koreans","Lithuanians","Magyars","Malay","Malians","Mayans","Mongols","Persians","Portuguese","Saracens","Slavs","Spanish","Tatars","Teutons","Turks","Vietnamese","Vikings"]
 
-#Print random choice from civilizations list
-print(random.choice(civilizations))
+#User inputs how many people are playing
+print('I hear you need help choosing civlizations for Age of Empires II. How many players?')
+x = input()
+for i in range(int(x)):
+    #Print random choice from civilizations list
+    print('Player ' + str(int(i+1)) + ' shall play as ' + random.choice(civilizations) + '.')
