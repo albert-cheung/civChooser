f469d2e8fbf0ceba2bccadecd6720e50854c838d
albert-cheung <43798492+albert-cheung@users.noreply.github.com>
2020-08-26 23:11:46 -0500

    Create picker.py

---
+++ picker.py
@@ -0,0 +1,7 @@
+import random
+
+#List of civilizations
+civilizations = ["Aztecs","Berbers","Britons","Bulgarians","Burmese","Byzantines","Celts","Chinese","Cumans","Ethiopians","Franks","Goths","Huns","Incas","Indians","Italians","Japanese","Khmer","Koreans","Lithuanians","Magyars","Malay","Malians","Mayans","Mongols","Persians","Portuguese","Saracens","Slavs","Spanish","Tatars","Teutons","Turks","Vietnamese","Vikings"]
+
+#Print random choice from civilizations list
+print(random.choice(civilizations))
