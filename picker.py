11dc2a0eaded39fa7118ef22804a2ee721ff182f
albert-cheung <43798492+albert-cheung@users.noreply.github.com>
2020-08-27 21:24:01 -0500

    Solving the issue of providing a number

--- picker.py
+++ picker.py
@@ -4,8 +4,11 @@ import random
 civilizations = ["Aztecs","Berbers","Britons","Bulgarians","Burmese","Byzantines","Celts","Chinese","Cumans","Ethiopians","Franks","Goths","Huns","Incas","Indians","Italians","Japanese","Khmer","Koreans","Lithuanians","Magyars","Malay","Malians","Mayans","Mongols","Persians","Portuguese","Saracens","Slavs","Spanish","Tatars","Teutons","Turks","Vietnamese","Vikings"]
 
 #User inputs how many people are playing
-print('I hear you need help choosing civlizations for Age of Empires II. How many players?')
+print('I hear you need help choosing civlizations for Age of Empires II. How many players need help assistance choosing a civilization?')
 x = input()
-for i in range(int(x)):
-    #Print random choice from civilizations list
-    print('Player ' + str(int(i+1)) + ' shall play as ' + random.choice(civilizations) + '.')
+if int(x) < 1:
+    print("You can't have less than one player. Please uninstall the game.")
+else:
+    for i in range(int(x)):
+        #Print random choice from civilizations list
+        print('Player ' + str(int(i+1)) + ' shall play as ' + random.choice(civilizations) + '.')
